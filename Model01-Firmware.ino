b72cfac597f07bf7239a7cc97b2c624c6e96c886
Tom Kirchner <git@halffull.org>
2017-10-29 21:46:43 -0700

    Layout The Second
    I couldn't get used to the escape key on the thumb.
    Still not sure I'll be able to do backspace on the thumb.

--- Model01-Firmware.ino
+++ Model01-Firmware.ino
@@ -10,9 +10,6 @@
 // The Kaleidoscope core
 #include "Kaleidoscope.h"
 
-// Allows modifiers to serve a different purpose when tapped
-#include <Kaleidoscope-SpaceCadet.h>
-
 // Support for macros
 #include "Kaleidoscope-Macros.h"
 
@@ -45,32 +42,32 @@ enum { QWERTY, FUNCTION }; // layers
 const Key keymaps[][ROWS][COLS] PROGMEM = {
 
   [QWERTY] = KEYMAP_STACKED
-  (Key_Backtick, Key_1, Key_2, Key_3, Key_4, Key_5, Key_Home,
-   Key_Tab,      Key_Q, Key_W, Key_E, Key_R, Key_T, Key_Enter,
-   Key_PageUp,   Key_A, Key_S, Key_D, Key_F, Key_G,
-   Key_PageDown, Key_Z, Key_X, Key_C, Key_V, Key_B, Key_Delete,
-   Key_LeftControl, Key_LeftGui, Key_LeftShift, Key_LeftAlt,
+  (Key_Escape,      Key_1, Key_2, Key_3, Key_4, Key_5, Key_Equals,
+   Key_Tab,         Key_Q, Key_W, Key_E, Key_R, Key_T, Key_LeftBracket,
+   Key_LeftShift,   Key_A, Key_S, Key_D, Key_F, Key_G,
+   Key_Backtick,    Key_Z, Key_X, Key_C, Key_V, Key_B, Key_Home,
+   Key_LeftControl, Key_Backspace, Key_LeftGui, Key_LeftAlt,
    ShiftToLayer(FUNCTION),
 
-   Key_End,        Key_6, Key_7, Key_8,     Key_9,      Key_0,         Key_Minus,
-   Key_Enter,      Key_Y, Key_U, Key_I,     Key_O,      Key_P,         Key_Equals,
-                   Key_H, Key_J, Key_K,     Key_L,      Key_Semicolon, Key_Quote,
-   Key_Backspace,  Key_N, Key_M, Key_Comma, Key_Period, Key_Slash,     Key_Backslash,
-   Key_RightAlt, Key_RightShift, Key_Spacebar, Key_RightControl,
+   Key_LEDEffectNext, Key_6, Key_7, Key_8,     Key_9,      Key_0,         Key_Minus,
+   Key_RightBracket,  Key_Y, Key_U, Key_I,     Key_O,      Key_P,         Key_Quote,
+                      Key_H, Key_J, Key_K,     Key_L,      Key_Semicolon, Key_RightShift,
+   Key_End,           Key_N, Key_M, Key_Comma, Key_Period, Key_Slash,     Key_Backslash,
+   Key_Delete, Key_Enter, Key_Spacebar, Key_RightControl,
    ShiftToLayer(FUNCTION)),
 
   [FUNCTION] =  KEYMAP_STACKED
-  (Key_Sysreq, Key_F1,            Key_F2, Key_F3, Key_F4,                Key_F5, Key_LEDEffectNext,
-   ___,        ___,               ___,    ___,    ___,                   ___,    ___,
-   ___,        ___,               ___,    ___,    ___,                   ___,
-   ___,        ___,               ___,    ___,    M(MACRO_VERSION_INFO), ___,    Key_Backspace,
-   ___,        Key_PcApplication, ___,    ___,
+  (Key_Sysreq, Key_F1,     Key_F2,            Key_F3, Key_F4,                Key_F5, ___,
+   ___,        ___,        ___,               ___,    ___,                   ___,    ___,
+   ___,        ___,        ___,               ___,    ___,                   ___,
+   ___,        ___,        ___,               ___,    M(MACRO_VERSION_INFO), ___,    Key_PageUp,
+   ___,        Key_Delete, Key_PcApplication, ___,
    ___,
 
-   Consumer_ScanPreviousTrack, Key_F6,                 Key_F7,        Key_F8,                   Key_LeftBracket,          Key_RightBracket, Key_F11,
+   Consumer_ScanPreviousTrack, Key_F6,                 Key_F7,        Key_F8,                   Key_F9,                   Key_F10,          Key_F11,
    Consumer_PlaySlashPause,    Consumer_ScanNextTrack, ___,           Key_Insert,               ___,                      Key_PrintScreen,  Key_F12,
                                Key_LeftArrow,          Key_DownArrow, Key_UpArrow,              Key_RightArrow,           ___,              ___,
-   Key_Delete,                 ___,                    Key_Mute,      Consumer_VolumeDecrement, Consumer_VolumeIncrement, ___,              ___,
+   Key_PageDown,               ___,                    Key_Mute,      Consumer_VolumeDecrement, Consumer_VolumeIncrement, ___,              ___,
    ___, ___, ___, ___,
    ___)
 };
@@ -132,25 +129,8 @@ void setup() {
     &solidRed, &solidOrange, &solidYellow, &solidGreen, &solidBlue, &solidIndigo, &solidViolet,
 
     // The macros plugin adds support for macros
-    &Macros,
-
-    // SpaceCadet allows modifiers to be tapped to serve as another key
-    &SpaceCadet
+    &Macros
   );
-
-  // Set the keymap with a timeout per-key
-  // Setting is {KeyThatWasPressed, AlternativeKeyToSend, TimeoutInMS}
-  // Note: must end with the SPACECADET_MAP_END delimiter
-  static kaleidoscope::SpaceCadet::KeyBinding spacecadetmap[] = {
-      {Key_LeftGui, Key_Escape, 500}
-    , {Key_LeftAlt, Key_LeftCurlyBracket, 500}
-    , {Key_RightAlt, Key_RightCurlyBracket, 500}
-    , {Key_LeftControl, Key_LeftBracket, 500}
-    , {Key_RightControl, Key_RightBracket, 500}
-    , SPACECADET_MAP_END
-  };
-  // Set the map.
-  SpaceCadet.map = spacecadetmap;
 
   // We set the brightness of the rainbow effects to 150 (on a scale of 0-255)
   // This draws more than 500mA, but looks much nicer than a dimmer effect
